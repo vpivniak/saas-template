d44034fd230452472e44c52ec2a7f81a65ef0be5
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2018-01-02 21:43:00 +0000

    docker-compose for java service

---
+++ up.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+envFile="./.env"
+
+if [ -f "$envFile" ]
+then
+  . $envFile
+
+  # [+] service
+  pushd services/java
+  export SERVICE_ARTIFACT=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.artifactId}-${project.version}-jar-with-dependencies.${project.packaging}' --non-recursive org.codehaus.mojo:exec-maven-plugin:1.3.1:exec)
+  popd
+  # [-] service
+
+  docker-compose up $1
+
+else
+  echo "'$envFile' not found."
+  echo "copy '.env.template' to '$envFile' and update it according to your environment"
+fi
+
