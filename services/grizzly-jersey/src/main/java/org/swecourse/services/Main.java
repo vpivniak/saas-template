395f9365ef4157080e178c787657c81a648bc3e2
Vladyslav Kurmaz <vladislav.kurmaz@gmail.com>
2018-02-14 12:41:08 +0200

    Update Main.java

--- services/grizzly-jersey/src/main/java/org/swecourse/services/Main.java
+++ services/grizzly-jersey/src/main/java/org/swecourse/services/Main.java
@@ -27,13 +27,14 @@ public class Main {
   private static final Optional < String > host;
   private static final Optional < String > port;
 
-  public static Integer i = new Integer()
+  public static Integer i = new Integer
   
   static {
     PROTOCOL = "http://";
     host = Optional.ofNullable(System.getenv("SERVICES_GJ_HOSTNAME"));
     port = Optional.ofNullable(System.getenv("SERVICES_GJ_PORT"));
     BASE_URI = PROTOCOL + host.orElse("localhost") + ":" + port.orElse("80") + "/";
+    i = 5;
   }
 
   /**
